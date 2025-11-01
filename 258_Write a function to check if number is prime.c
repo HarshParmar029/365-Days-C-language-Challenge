b522c331131aba7be33b0cd2e0a040739f5cd119
029_HarshParmar <hp259369@gmail.com>
2025-11-01 23:58:25 +0530

    Add files via upload

---
+++ 258_Write a function to check if number is prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function to check if a number is prime
+int isPrime(int n) {
+    if (n <= 1)
+        return 0; // Not prime
+
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return 0; // Not prime
+    }
+    return 1; // Prime
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPrime(num))
+        printf("%d is a prime number.", num);
+    else
+        printf("%d is not a prime number.", num);
+
+    return 0;
+}
+
