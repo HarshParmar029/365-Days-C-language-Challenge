b49c64cbda1379c4f0e6eec06b9fee7fd02f2749
029_HarshParmar <hp259369@gmail.com>
2025-11-02 22:36:04 +0530

    Add files via upload

---
+++ 272_Write a function to find maximum in array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Function to find maximum element in array
+int findMax(int arr[], int n) {
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return max;
+}
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = findMax(arr, n);
+
+    printf("Maximum element = %d\n", max);
+    return 0;
+}
