b3e9d0967b18ec3bf74a23905f143ffb50a74fd7
029_HarshParmar <hp259369@gmail.com>
2025-11-03 23:15:05 +0530

    Add files via upload

---
+++ 281_Write a function to add two matrices.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+// Function to add two matrices
+void addMatrices(int a[10][10], int b[10][10], int result[10][10], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+// Function to display a matrix
+void displayMatrix(int matrix[10][10], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main() {
+    int a[10][10], b[10][10], result[10][10];
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            scanf("%d", &a[i][j]);
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            scanf("%d", &b[i][j]);
+    addMatrices(a, b, result, rows, cols);
+    printf("\nResultant Matrix after Addition:\n");
+    displayMatrix(result, rows, cols);
+    return 0;
+}
