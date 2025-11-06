ef526ceeb5966727542921a803cda8e23092ec69
029_HarshParmar <hp259369@gmail.com>
2025-11-06 23:13:22 +0530

    Add files via upload

---
+++ the function with no argument and no written value.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void displayMes();
+int main() {
+    MSG();
+    return 0;
+}
+void MSG() {
+    printf("Hello! This is a function with no arguments and no return value.\n");
+}
