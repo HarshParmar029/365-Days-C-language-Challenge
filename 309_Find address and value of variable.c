b9fed2ec201bcbbabde9ec529ee7a2acd5fccba0
029_HarshParmar <hp259369@gmail.com>
2025-11-09 18:36:42 +0530

    Add files via upload

---
+++ 309_Find address and value of variable.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int num = 25;   // variable declaration and initialization
+    printf("Value of variable num = %d\n", num);
+    printf("Address of variable num = %p\n", &num);
+    return 0;
+}
